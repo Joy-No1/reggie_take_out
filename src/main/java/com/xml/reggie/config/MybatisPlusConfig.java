ccbcb9ac330a8a24747f5434b3bf04d29ad9fd0e
Joy <11525357+joyxml@user.noreply.gitee.com>
2022-09-08 17:06:03 +0800

    分页查询配置类

--- src/main/java/com/xml/reggie/config/MybatisPlusConfig.java
+++ src/main/java/com/xml/reggie/config/MybatisPlusConfig.java
@@ -2,9 +2,16 @@ package com.xml.reggie.config;
 
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+/**
+ * 分页查询配置类
+ */
+@Configuration
 public class MybatisPlusConfig {
 
+    @Bean
     public MybatisPlusInterceptor mybatisPlusInterceptor(){
         MybatisPlusInterceptor mybatisPlusInterceptor=new MybatisPlusInterceptor();
         mybatisPlusInterceptor.addInnerInterceptor(new PaginationInnerInterceptor());
