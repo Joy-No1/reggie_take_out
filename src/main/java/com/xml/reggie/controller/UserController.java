a571edc6c1c380eaba59d87b0788b90def644a9b
Joy <11525357+joyxml@user.noreply.gitee.com>
2022-09-19 21:42:50 +0800

    验证码业务bug修改完成

--- src/main/java/com/xml/reggie/controller/UserController.java
+++ src/main/java/com/xml/reggie/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.xml.reggie.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.xml.reggie.common.R;
 import com.xml.reggie.entity.User;
 import com.xml.reggie.service.UserService;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpSession;
+import java.util.Map;
 
 @RestController
 @Slf4j
@@ -26,20 +28,52 @@ public class UserController {
 
     /**
      * 发送验证码短信
+     *
      * @param user
      * @return
      */
     @PostMapping("/sendMsg")
-    public R<String> sendMsg(@RequestBody User user, HttpSession session){
+    public R<String> sendMsg(@RequestBody User user, HttpSession session) {
         String phone = user.getPhone();
-        if (StringUtils.isNotEmpty(phone)){
-            String code = ValidateCodeUtils.generateValidateCode(4).toString();
-            log.info("code:{}",code);
+        if (StringUtils.isNotEmpty(phone)) {
+            String code = ValidateCodeUtils.generateValidateCode(6).toString();
+            log.info("code:{}", code);
 //            SMSUtils.sendMessage("瑞吉外卖","",phone,code);
-            session.setAttribute("phone",code);
+            session.setAttribute(phone, code);
             return R.success("手机验证码短信发送成功!");
         }
         return R.error("短信发送失败");
     }
 
+
+    /**
+     * 移动端用户登录
+     *
+     * @param map
+     * @param session
+     * @return
+     */
+    @PostMapping("/login")
+    public R<User> login(@RequestBody Map map, HttpSession session) {
+        log.info("map:{}", map);
+        String phone = map.get("phone").toString();
+        String code = map.get("code").toString();
+        Object codeInSession = session.getAttribute(phone);
+        if (codeInSession != null && codeInSession.equals(code)) {
+            LambdaQueryWrapper<User> queryWrapper = new LambdaQueryWrapper<>();
+            queryWrapper.eq(User::getPhone, phone);
+            User user = userService.getOne(queryWrapper);
+            if (user == null) {
+                user = new User();
+                user.setPhone(phone);
+                user.setStatus(1);
+                userService.save(user);
+            }
+            session.setAttribute("user", user.getId());
+            return R.success(user);
+        }
+        return R.error("登陆失败,用户名或密码错误");
+    }
 }
+
+
