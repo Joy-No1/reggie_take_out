7a93cc0d01ab78e3653b7881e551aa5cc26b961e
Joy <11525357+joyxml@user.noreply.gitee.com>
2022-09-27 21:16:23 +0800

    购物车开发完善 Bug待修复

--- src/main/java/com/xml/reggie/controller/ShoppingCartController.java
+++ src/main/java/com/xml/reggie/controller/ShoppingCartController.java
@@ -1,13 +1,122 @@
 package com.xml.reggie.controller;
 
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.xml.reggie.common.BaseContext;
+import com.xml.reggie.common.R;
+import com.xml.reggie.entity.Setmeal;
+import com.xml.reggie.entity.ShoppingCart;
+import com.xml.reggie.service.ShoppingCartService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @RestController
 @Slf4j
 @RequestMapping("/shoppingCart")
 public class ShoppingCartController {
 
+    @Autowired
+    private ShoppingCartService shoppingCartService;
+
+    /**
+     * 添加购物车
+     *
+     * @param shoppingCart
+     * @return
+     */
+    @PostMapping("/add")
+    public R<ShoppingCart> add(@RequestBody ShoppingCart shoppingCart) {
+        log.info("shoppingCart:{}", shoppingCart);
+        //设置用户ID
+        shoppingCart.setUserId(BaseContext.getCurrentId());
+
+        //查询当前菜品或者套餐是否在购物车
+        Long dishId = shoppingCart.getDishId();
+        LambdaQueryWrapper<ShoppingCart> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(ShoppingCart::getUserId, BaseContext.getCurrentId());
+        if (dishId != null) {
+            queryWrapper.eq(ShoppingCart::getDishId, dishId);
+        } else {
+            queryWrapper.eq(ShoppingCart::getSetmealId, shoppingCart.getSetmealId());
+        }
+        ShoppingCart cartServiceOne = shoppingCartService.getOne(queryWrapper);
+        if (cartServiceOne != null) {
+            //存在 在原来数量基础上+1
+            Integer number = cartServiceOne.getNumber();
+            cartServiceOne.setNumber(number + 1);
+            shoppingCartService.updateById(cartServiceOne);
+        } else {
+            //不存在 添加到购物车 数量默认1
+            shoppingCart.setNumber(1);
+            shoppingCart.setCreateTime(LocalDateTime.now());
+            shoppingCartService.save(shoppingCart);
+            cartServiceOne = shoppingCart;
+        }
+        return R.success(cartServiceOne);
+    }
+
+
+    /**
+     * 查询用户购物车列表
+     *
+     * @return
+     */
+    @GetMapping("/list")
+    public R<List<ShoppingCart>> list() {
+        log.info("查看购物车!!!");
+        LambdaQueryWrapper<ShoppingCart> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(ShoppingCart::getUserId, BaseContext.getCurrentId()).orderByAsc(ShoppingCart::getCreateTime);
+        List<ShoppingCart> list = shoppingCartService.list(queryWrapper);
+        return R.success(list);
+    }
+
+
+    /**
+     * 清空购物车
+     *
+     * @return
+     */
+    @DeleteMapping("/clean")
+    public R<String> clean() {
+
+        LambdaQueryWrapper<ShoppingCart> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(ShoppingCart::getUserId, BaseContext.getCurrentId());
+        shoppingCartService.remove(queryWrapper);
+        return R.success("清空购物车成功!");
+    }
+
+    /**
+     * 取消加入购物车
+     *
+     * @param shoppingCart
+     * @return
+     */
+    @PostMapping("/sub")
+    public R<ShoppingCart> sub(@RequestBody ShoppingCart shoppingCart) {
+        log.info("shoppingCart{}", shoppingCart);
+        LambdaQueryWrapper<ShoppingCart> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(ShoppingCart::getUserId, BaseContext.getCurrentId());
+        Long dishId = shoppingCart.getDishId();
+        if (dishId != null) {
+            queryWrapper.eq(ShoppingCart::getDishId, dishId);
+        } else {
+            queryWrapper.eq(ShoppingCart::getSetmealId, shoppingCart.getSetmealId());
+        }
+        ShoppingCart cartServiceOne = shoppingCartService.getOne(queryWrapper);
+        if (cartServiceOne != null) {
+            Integer number = cartServiceOne.getNumber();
+
+            if (number >= 1) {
+                //数量-1
+                cartServiceOne.setNumber(number - 1);
+            }
+
+            shoppingCartService.updateById(cartServiceOne);
+        }
+        return R.success(cartServiceOne);
+    }
 }
