ecf311d719526c183610a16f82e6e75df95f9924
Joy <11525357+joyxml@user.noreply.gitee.com>
2022-09-28 16:32:58 +0800

    用户下单完善

---
+++ src/main/java/com/xml/reggie/service/impl/OrderServiceImpl.java
@@ -0,0 +1,95 @@
+package com.xml.reggie.service.impl;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.IdWorker;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.xml.reggie.common.BaseContext;
+import com.xml.reggie.common.CustomException;
+import com.xml.reggie.entity.*;
+import com.xml.reggie.mapper.OrderMapper;
+import com.xml.reggie.service.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+
+@Service
+public class OrderServiceImpl extends ServiceImpl<OrderMapper, Orders> implements OrderService {
+
+    @Autowired
+    private ShoppingCartService shoppingCartService;
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private AddressBookService addressBookService;
+
+    @Autowired
+    private OrderDetailService orderDetailService;
+
+    /**
+     * 用户下单
+     *
+     * @param orders
+     */
+    @Transactional
+    @Override
+    public void submit(Orders orders) {
+        Long userId = BaseContext.getCurrentId();
+
+        LambdaQueryWrapper<ShoppingCart> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(ShoppingCart::getUserId, userId);
+        List<ShoppingCart> shoppingCarts = shoppingCartService.list(queryWrapper);
+        if (shoppingCarts == null || shoppingCarts.size() == 0) {
+            throw new CustomException("购物车为空,不能下单");
+        }
+        User user = userService.getById(userId);
+        Long addressBookId = orders.getAddressBookId();
+        AddressBook addressBook = addressBookService.getById(addressBookId);
+        if (addressBook == null) {
+            throw new CustomException("地址信息有误,不能下单");
+        }
+        long orderId = IdWorker.getId();
+        AtomicInteger amount = new AtomicInteger(0);
+        List<OrderDetail> orderDetails = shoppingCarts.stream().map((item) -> {
+            OrderDetail orderDetail = new OrderDetail();
+            orderDetail.setOrderId(orderId);
+            orderDetail.setNumber(item.getNumber());
+            orderDetail.setDishFlavor(item.getDishFlavor());
+            orderDetail.setDishId(item.getDishId());
+            orderDetail.setSetmealId(item.getSetmealId());
+            orderDetail.setName(item.getName());
+            orderDetail.setImage(item.getImage());
+            orderDetail.setAmount(item.getAmount());
+            amount.addAndGet(item.getAmount().multiply(new BigDecimal(item.getNumber())).intValue());
+            return orderDetail;
+        }).collect(Collectors.toList());
+
+
+        orders.setNumber(String.valueOf(orderId));
+        orders.setOrderTime(LocalDateTime.now());
+        orders.setCheckoutTime(LocalDateTime.now());
+        orders.setStatus(2);
+        orders.setAmount(new BigDecimal(amount.get()));
+        orders.setUserId(userId);
+        orders.setUserName(user.getName());
+        orders.setConsignee(addressBook.getConsignee());
+        orders.setPhone(addressBook.getPhone());
+        orders.setAddress((addressBook.getProvinceName() == null ? "" : addressBook.getProvinceName())
+                + (addressBook.getCityName() == null ? "" : addressBook.getCityName())
+                + (addressBook.getDistrictName() == null ? "" : addressBook.getDistrictName())
+                + (addressBook.getDetail() == null ? "" : addressBook.getDetail()));
+
+        orderDetailService.saveBatch(orderDetails);
+        this.save(orders);
+        shoppingCartService.remove(queryWrapper);
+    }
+
+
+}
